6a3a4b4d3ae5b59b82a316d7f5fce39545093d1b
mtungij <jamesmtungi2013@gmail.com>
2024-07-27 09:52:59 +0300

    first commit

--- application/controllers/Super_admin.php
+++ application/controllers/Super_admin.php
@@ -5,4 +5,30 @@ class Super_admin extends CI_Controller {
 	{
 		$this->load->view('super/index');
 	}
+
+
+
+
+public function register_company(){
+	           
+		
+	$data = $this->input->post();
+
+   //  print_r($data);
+   //  exit();
+   
+	$data['password'] = sha1($this->input->post('password'));
+	//$data['comp_phone'] = ('255'.$this->input->post('comp_phone'));
+	 //   echo "<pre>";
+	 // print_r($data);
+	 // echo "</pre>";
+	 //      exit();
+	   $this->load->model('queries');
+	  $this->queries->insert_company_data($data);
+		  
+	 
+	 return redirect('welcome/index');
 }
+
+}
+
